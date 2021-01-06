63d2007a8bd090f9110854639c9be3b7df88a958
Catalin Palade <catalin@recruitd.com>
2021-01-06 14:32:04 +0000

    todo item coredata entity + class

---
+++ TodoList_CoreData/TodoItem/TodoItem+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  TodoItem+CoreDataClass.swift
+//  TodoList_CoreData
+//
+//  Created by Catalin Palade on 06/01/2021.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(TodoItem)
+public class TodoItem: NSManagedObject {
+
+}
---
+++ TodoList_CoreData/TodoItem/TodoItem+CoreDataProperties.swift
@@ -0,0 +1,28 @@
+//
+//  TodoItem+CoreDataProperties.swift
+//  TodoList_CoreData
+//
+//  Created by Catalin Palade on 06/01/2021.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension TodoItem {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<TodoItem> {
+        return NSFetchRequest<TodoItem>(entityName: "TodoItem")
+    }
+
+    @NSManaged public var taskTitle: String?
+    @NSManaged public var taskDescription: String?
+    @NSManaged public var taskDate: Date?
+    @NSManaged public var isCompleted: Bool
+
+}
+
+extension TodoItem : Identifiable {
+
+}
